efda1aff013a7e82a453f1ff6724a2c619889644
Gabriel Katz <gabrielkatz@Gabriels-MacBook-Pro.local>
2018-01-16 15:07:48 -0800

    [ops] Basic scaffolding and webpack setup

---
+++ client/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'HRLA Scheduler webpack setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
---
+++ webpack.dev.config.js
@@ -0,0 +1,51 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+const APP_DIR = path.resolve(__dirname, './client/template.index.html');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+const ENV_DIR = path.resolve(__dirname, './client/.env')
+const TEMPLATE_DIR = require('./client/template.index.html');
+
+
+const cleanOptions = {
+  verbose: true,
+};
+module.exports = {
+  entry: {
+    app: APP_DIR,
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], cleanOptions),
+    new HTMLWebpackPlugin({
+      template: TEMPLATE_DIR,
+      inject: 'body',
+    }),
+    new Dotenv({
+      path: ENV_DIR,
+      safe: false,
+    })
+  ]
+}
